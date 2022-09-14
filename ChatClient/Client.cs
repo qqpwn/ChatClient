85ad3ed9bfe705fafddfd927637515144c0918b0
Maximilian Kristensen <er4072@hotmail.com>
2022-09-14 14:27:46 +0200

    Add project files.

---
+++ ChatClient/Client.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ChatClient
+{
+
+    class Client
+    {
+
+        static void Main(string[] args)
+        {
+            ExecuteClient();
+
+        }
+
+        static void ExecuteClient()
+        {
+
+            try
+            {
+
+                IPHostEntry ipHost = Dns.GetHostEntry(Dns.GetHostName());
+                IPAddress ipAddr = ipHost.AddressList[0];
+                IPEndPoint iPEndPoint = new IPEndPoint(ipAddr, 11111);
+
+                Socket sender = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+                try
+                {
+
+                    sender.Connect(iPEndPoint);
+
+                    Console.WriteLine("Socket conneted to -> {0} ", sender.RemoteEndPoint.ToString());
+
+                    byte[] messageSent = Encoding.ASCII.GetBytes("Test Client");
+                    int bytesSent = sender.Send(messageSent);
+
+                    byte[] messageReceived = new byte[1024];
+
+                    int bytesRecv = sender.Receive(messageReceived);
+                    Console.WriteLine("Message from server -> {0}", Encoding.ASCII.GetString(messageReceived, 0, bytesRecv));
+
+                    sender.Shutdown(SocketShutdown.Both);
+                    sender.Close();
+
+                }
+                catch (ArgumentNullException ane)
+                {
+
+                    Console.WriteLine("ArgumentNullException : {0}", ane.ToString()); ;
+                }
+                catch (SocketException anr)
+                {
+                    Console.WriteLine("SocketException : {0}", anr.ToString());
+                }
+                catch (Exception e) 
+                {
+                    Console.WriteLine("Unexpected Exception {0}", e.ToString());
+                }
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.ToString()); ;
+            }
+        }
+
+
+    }
+}
